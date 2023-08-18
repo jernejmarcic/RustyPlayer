c86f818c2629fc376c67065906a8bbda6c804b4d
jernejmarcic <91026972+jernejmarcic@users.noreply.github.com>
2023-08-18 14:09:19 +0200

    Album art support

--- README.md
+++ README.md
@@ -7,6 +7,26 @@
 - [x] Next
 - [x] Shuffle
 - [ ] Previous song (the foundaton exists)
-- [ ] Lopping
+- [x] Lopping
+- [ ] mpris support
+- [x] Album art
+
+### Planned Features
+
+ 1. mpris (d-bus) support
+ 2. Audio visualizer
+ ~~3. Album images~~
+ 4. Lyrics
+ 5. Better TUI
+ 6. More (some) CLI options
+ 8. Better documentation
+
+## Limitations
+ - It's my first project
+ - The code is messy
+ - poor UI
+ - Too much Chat GPT
+
+
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,25 @@
 use audiotags::Tag;
-use dbus::blocking::Connection;
-use dbus::message::MatchRule;
-use device_query::{DeviceQuery, DeviceState, Keycode};
-use indicatif::ProgressBar;
 use rand::rngs::ThreadRng;
 use rand::Rng;
-use rodio::source::{SineWave, Source};
 use rodio::{Decoder, OutputStream, Sink};
-use souvlaki::{MediaControlEvent, MediaControls, MediaMetadata, PlatformConfig};
-use std::fs::read;
 use std::fs::File;
 use std::io::BufReader;
-use std::io::{self, empty};
 use std::path::PathBuf;
-use std::process::Command;
-use std::sync::mpsc::channel;
-use std::sync::mpsc::Receiver;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
-use std::thread::sleep;
-use std::time::Duration;
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 use walkdir::WalkDir;
-use walkdir::*;
+use std::sync::mpsc::{self, TryRecvError};
+use std::{thread, time::Duration};
+use std::io;
+use termion::raw::IntoRawMode;
+use std::process::Command;
+use std::path::Path;
 
-//struct PlayerControl {
-//
-//    rx: Receiver<MediaControlEvent>,
-//   sink: Arc<Mutex<Sink>>,  // Use an Arc<Mutex<Sink>> to share the Sink safely across threads
-//}
-
-//impl PlayerControl {
-//    pub fn new(sink: Sink) -> Self {
-//        let config = PlatformConfig {
-//            dbus_name: "oxiplayer",
-//            display_name: "Oxiplayer",
-//            hwnd: None,
-//       };
-//
-//        let mut controls = MediaControls::new(config).unwrap();
-//       let (tx, rx) = channel();
-//
-//        controls
-//            .attach(move |event| tx.send(event).unwrap())
-//            .unwrap();
-//
-//        Self {
-//           controls,
-//            rx,
-//            sink: Arc::new(Mutex::new(sink)),
-//        }
-//    }
-//
-//    pub fn listen(&self) {
-//        while let Ok(event) = self.rx.recv() {
-//            match event {
-//                MediaControlEvent::Toggle => {
-//                    let mut sink = self.sink.lock().unwrap();
-//                    sink.pause()
-//                }
-//                MediaControlEvent::Next => {
-//                //;
-//                }
-//                MediaControlEvent::Previous => {
-//
-//                }
-//                // ... Handle other events as required
-//                _ => {}
-//            }
-//        }
-//    }
-//}
 
 fn main() {
+    // Enable raw mode
+    let _stdout = io::stdout().into_raw_mode().unwrap();
+
     let paths: Vec<_> = WalkDir::new("/home/jernej/Music/Playlist")
         .into_iter()
         .filter_map(Result::ok)
@@ -83,33 +27,45 @@ fn main() {
         .collect();
 
     let mut played_songs: Vec<i32> = Vec::new();
-
     let mut rng = rand::thread_rng();
 
-    // Create the output stream and sink here
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    // Pass the sink to the PlayerControl::new method
-    //    let player_control = PlayerControl::new(sink);
-    //    start_mpris_listener(player_control);
-
-    looper(&paths, &mut played_songs, &mut rng);
+    let key_channel = spawn_stdin_channel();
+    looper(&paths, &mut played_songs, &mut rng, &key_channel);
 }
 
-fn looper(paths: &Vec<PathBuf>, played_songs: &mut Vec<i32>, rng: &mut ThreadRng) {
+
+fn looper(
+    paths: &Vec<PathBuf>,
+    played_songs: &mut Vec<i32>,
+    rng: &mut ThreadRng,
+    key_channel: &mpsc::Receiver<Key>
+) {
     loop {
         let randint = rng.gen_range(0..paths.len());
         played_songs.push(randint as i32);
-        println!("Song numbers played: {:?}", played_songs);
-        play_random_song(paths, randint, rng);
+        // println!("Song numbers played: {:?} \r", played_songs);
+        play_random_song(paths, randint, rng, key_channel);
     }
 }
 
-fn play_random_song(paths: &Vec<PathBuf>, randint: usize, rng: &mut ThreadRng) {
+fn play_random_song(
+    paths: &Vec<PathBuf>,
+    randint: usize,
+    rng: &mut ThreadRng,
+    key_channel: &mpsc::Receiver<Key>
+) {
     clearscreen::clear().expect("failed to clear screen");
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let tag = Tag::new().read_from_path(&paths[randint]).unwrap();
+
+    // Extract and display the album cover
+    extract_cover_from_flac(&paths[randint]).unwrap();
+    display_image_as_ascii("cover.jpg").unwrap();
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
     let tag = Tag::new().read_from_path(&paths[randint]).unwrap();
     let title = tag.title().unwrap_or_else(|| "Unknown".into());
     let artists = tag
@@ -118,57 +74,98 @@ fn play_random_song(paths: &Vec<PathBuf>, randint: usize, rng: &mut ThreadRng) {
         .unwrap_or_else(|| "Unknown".into());
     let album = tag.album_title().unwrap_or_else(|| "Unknown".into());
 
-    println!("Title:     {}", title);
-    println!("Artists:   {}", artists);
-    println!("Album:     {}", album);
+    println!("Title:     {} \r", title);
+    println!("Artists:   {} \r", artists);
+    println!("Album:     {} \r", album);
 
     let file = BufReader::new(File::open(&paths[randint]).unwrap());
     let source = Decoder::new(file).unwrap();
     sink.append(source);
 
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
-    let stdin = io::stdin();
-    let mut keys = stdin.keys();
-    loop {
-        if sink.empty() {
-            break;
-        }
+    while !sink.empty() == true {
+        match key_channel.try_recv() {
+            Ok(key) => handle_key_input(key, &sink),
+            Err(TryRecvError::Empty) => {},
+            Err(TryRecvError::Disconnected) => {
+                eprintln!("Key input channel disconnected");
+            }
 
-        let key = match keys.next() {
-            Some(result) => match result {
-                Ok(k) => k,
-                Err(_) => continue, // handle the error as you see fit
-            },
-            None => continue,
-        };
-
-        match key {
-            Key::Char('p') => {
-                // Play/Pause toggle
-                if sink.is_paused() {
-                    sink.play();
-                } else {
-                    sink.pause();
-                }
-            }
-            Key::Char('n') => {
-                return;
-            }
-            Key::Char('b') => {
-                // Your logic for the 'b' key
-                break;
-            }
-            _ => continue,
         }
+        thread::sleep(Duration::from_millis(100));
     }
-
-    // The sound plays in a separate thread. This call will block the current thread until the sink
-    // has finished playing all its queued sounds.
-    sink.sleep_until_end();
 }
 
-//fn start_mpris_listener(player_control: PlayerControl) {
-//    thread::spawn(move || {
-//        player_control.listen();
-//   });
-//}
+
+fn handle_key_input(key: Key, sink: &Sink) {
+    let _stdout = io::stdout().into_raw_mode().unwrap();
+    match key {
+        Key::Char('p') => {
+            if sink.is_paused() {
+                sink.play();
+            } else {
+                sink.pause();
+            }
+        },
+        Key::Char('n') => {
+            // skip to next song logic
+            sink.stop();
+        },
+        Key::Char('b') => {
+            // go back to previous song logic
+            // depending on your implementation
+        },
+        _ => {}
+    }
+}
+
+fn spawn_stdin_channel() -> mpsc::Receiver<Key> {
+    let (tx, rx) = mpsc::channel::<Key>();
+    thread::spawn(move || {
+        let mut stdin = termion::async_stdin().keys();
+        loop {
+            if let Some(Ok(key)) = stdin.next() {
+                tx.send(key).unwrap();
+            } else {
+                // You could add a small sleep here if you wish to prevent busy-waiting
+                thread::sleep(Duration::from_millis(10));
+            }
+        }
+    });
+    rx
+}
+fn display_image_as_ascii(image_path: &str) -> Result<(), std::io::Error> {
+    let output = Command::new("chafa")
+        .arg("-s")
+        .arg("80x25") // Adjust the size as necessary
+        .arg(image_path)
+        .output()?;
+
+    if output.status.success() {
+        print!("{} \r", String::from_utf8_lossy(&output.stdout));
+        Ok(())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to display image",
+        ))
+    }
+}
+
+fn extract_cover_from_flac(flac_path: &Path) -> Result<(), std::io::Error> {
+    let output_path = "cover.jpg"; // You can customize this
+    let output = Command::new("metaflac")
+        .arg("--export-picture-to")
+        .arg(output_path)
+        .arg(flac_path)
+        .output()?;
+
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to extract album cover",
+        ))
+    }
+}
+
